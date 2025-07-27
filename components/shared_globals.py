79d6ca2ad17ca2b5b274a1596d84511b06fc75c7
Khushi R. <ranikhushi5005e@gmail.com>
2025-07-27 12:49:11 +0530

    Create shared_globals.py

---
+++ components/shared_globals.py
@@ -0,0 +1,8 @@
+# components/shared_globals.py
+
+# Move GLOBAL_RESOURCES here
+GLOBAL_RESOURCES = {
+    "Helpline Numbers": "📞 Call 9152987821 or 9152987820 (iCall)",
+    "Mental Health NGO": "🧠 Visit mind.org.in",
+    "Suicide Prevention": "💬 Chat at AASRA or call 91-22-27546669"
+}
